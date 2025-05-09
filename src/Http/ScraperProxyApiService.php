761575bbf9ee317b429c007153989542d1539156
Pisklov Denys <denys.pisklov@gmail.com>
2025-05-09 11:00:13 +0300

    Rename some vars

--- src/Http/ScraperProxyApiService.php
+++ src/Http/ScraperProxyApiService.php
@@ -4,17 +4,17 @@ namespace Src\Http;
 
 class ScraperProxyApiService
 {
-    private static function getFullUrl() : string
+    private static function getBaseUrl() : string
     {
         return "https://api.scraperapi.com/?api_key=" . $_ENV['SCRAPER_API_KEY'] . "&url=";
     }
 
-    public function proxyUrlSource(string $url): string
+    public function proxyUrlSource(string $urlSource): string
     {
-        if (!filter_var($url, FILTER_VALIDATE_URL)) {
-            throw new \InvalidArgumentException("Incorrect URL for proxying : $url");
+        if (!filter_var($urlSource, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException("Incorrect URL for proxying : $urlSource");
         }
 
-        return static::getFullUrl() . $url;
+        return static::getBaseUrl() . $urlSource;
     }
 }
